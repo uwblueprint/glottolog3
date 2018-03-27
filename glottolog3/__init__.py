316b84865a8e03f7877f7f49a159374c351fbb25
justinjnlim <justinjnlim@gmail.com>
2018-03-26 21:20:43 -0400

    Removed placeholder names for API endpoint
    Changed API endpoint to be named /search, and for the search parameter to be called q.
    A search yielding no languoids will now return an empty list.

--- glottolog3/__init__.py
+++ glottolog3/__init__.py
@@ -141,8 +141,8 @@ def main(global_config, **settings):
         views.languages,
         renderer='language/bpsearch_html.mako')
     config.add_route(
-        'glottolog.bp_api_search',
-        'bp/api/search')
+        'glottolog.search',
+        '/search')
     config.add_route(
         'glottolog.add_identifier',
         '/identifiers')
